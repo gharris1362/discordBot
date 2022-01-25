dd45bb0e9663bac993a3b21ef9e51b8fc20e9521
pete-wiley <pete.wiley.0428@gmail.com>
2022-01-24 22:29:31 -0600

    format

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,21 +1,21 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        console.log(`${interaction.user.tag} in #${interaction.channel.name} Triggered an interaction.`);
 
-    module.exports = {
-        name: 'interactionCreate',
-        async execute(interaction) {
-            console.log(`${interaction.user.tag} in #${interaction.channel.name} Triggered an interaction.`);
+        if (!interaction.isCommand()) {
+            return
+        };
 
-            
-    if (!interaction.isCommand()) return;
+        const command = interaction.client.commands.get(interaction.commandName);
 
-    const command = interaction.client.commands.get(interaction.commandName)
+        if (!command) return;
 
-    if (!command) return;
-
-    try {
-        await command.execute(interaction)
-    } catch(error) {
-        console.error(error)
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
+        try {
+            await command.execute(interaction)
+        } catch (error) {
+            console.error(error)
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
     }
+}
